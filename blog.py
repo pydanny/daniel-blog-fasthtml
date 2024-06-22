e9faae50ef993a499a55569de36604df999e8b44
Daniel Roy Greenfeld <62857+pydanny@users.noreply.github.com>
2024-06-22 10:25:26 +0100

    Cleanup

--- blog.py
+++ blog.py
@@ -31,12 +31,9 @@ class BlogPost():
 
     def __xt__(self):
         return Span(
-                H2(
-                    A(self.title, href=f"/posts/{self.slug}")
-                ),
+                H2(A(self.title, href=f"/posts/{self.slug}")),
                 P(self.description),
-                Small(
-                    Time(self.timestamp))
+                Small(Time(self.timestamp))
                 )
 
 
@@ -50,47 +47,33 @@ def list_posts(published: bool = True, posts_dirname="posts") -> list[dict]:
         posts.append(data)
 
     posts = [x for x in filter(lambda x: x["published"] is True, posts)]
-
     posts.sort(key=lambda x: x["date"], reverse=True)
     return [x for x in filter(lambda x: x["published"] is published, posts)]
 
 def Time(time: str) -> str:
+    """Placeholder"""
     return time
 
 @app.get("/")
 def articles():
-    articles = []
-    for post in list_posts():
-        articles.append(
-            BlogPost(
-                title=post["title"],
-                slug=post["slug"],
-                timestamp=post["date"],
-                description=post.get("description", "")
-            )
-        )
+    posts = [BlogPost(title=x["title"],slug=x["slug"],timestamp=x["date"],description=x.get("description", "")) for x in list_posts()]
     return Title("Daniel Roy Greenfeld"), Main(
         Section(
-            H1(f'All Articles ({len(articles)})'),
+            H1(f'All Articles ({len(posts)})'),
             P('Everything written by Daniel Roy Greenfeld for the past 19 years'),
-            *articles
+            *posts
         )
     )
 
 @app.get("/posts/{slug}")
 def article(slug: str):
-    post = [
-            x for x in filter(lambda x: x["slug"] == slug, list_posts())
-        ][0]
+    post = [x for x in filter(lambda x: x["slug"] == slug, list_posts())][0]
     content = pathlib.Path(f"posts/{slug}.md").read_text().split("---")[2]
     return Title(post["title"]), Main(
         A("Back to all articles", href="/"),
         Section(
             H1(post["title"]),
-            Div(
-                content,
-                cls="marked"
-            )
+            Div(content,cls="marked")
         )
     )
     
