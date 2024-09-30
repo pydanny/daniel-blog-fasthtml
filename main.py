66596639bf7092b212c44bbedb49f68c21f07c15
Daniel Roy Greenfeld <daniel@feldroy.com>
2024-09-30 22:06:21 +0100

    Compress into one Python module and annotate

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-import json
-import pathlib
 
-from fasthtml.common import *
-from components import *
-from contents import *
-
+import collections
 from datetime import datetime
+from dateutil import parser
+import functools
+import pathlib
+import json
+
+import pytz
+import yaml
+from fasthtml.common import *
+
+default_social_image = '/public/images/profile.jpg'
 
 # This redirects dict is a relic of previous blog migrations.
 # It is used to redirect old URLs embedded in books, presentations,
 # and more to the new locations.
-redirects = json.loads(pathlib.Path(f"redirects.json").read_text())   
+redirects = json.loads(pathlib.Path(f"redirects.json").read_text())  
 
 hdrs = (
     MarkdownJS(),
@@ -20,7 +25,148 @@ hdrs = (
     Link(rel='stylesheet', href='/public/style.css', type='text/css'),        
 )
 
+# The following functions are three content loading. They are cached in
+# memory to boost the speed of the site. In production at a minumum the
+# app is restarted every time the project is deployed.
+@functools.cache
+def list_posts(published: bool = True, posts_dirname="posts", content=False) -> list[dict]:
+    """
+    Loads all the posts and their frontmatter.
+    Note: Could use pathlib better
+    """
+    posts: list[dict] = []
+    for post in pathlib.Path('.').glob(f"{posts_dirname}/**/*.md"):
+        raw: str = post.read_text().split("---")[1]
+        data: dict = yaml.safe_load(raw)
+        data["slug"] = post.stem
+        if content:
+            data["content"] = '\n'.join(post.read_text().split("---")[2:])
+        posts.append(data)
+
+    posts.sort(key=lambda x: x["date"], reverse=True)
+    return [x for x in filter(lambda x: x["published"] is published, posts)]
+
+@functools.lru_cache
+def get_post(slug: str):
+    posts = list_posts(content=True)
+    post = next((x for x in posts if x["slug"] == slug), None)
+    return (post['content'], post)
+
+@functools.cache
+def list_tags() -> dict[str, int]:
+    unsorted_tags = {}
+    for post in list_posts():
+        page_tags = post.get("tags", [])
+        for tag in page_tags:
+            if tag in unsorted_tags:
+                unsorted_tags[tag] += 1
+            else:
+                unsorted_tags[tag] = 1  
+
+    tags: dict = collections.OrderedDict(
+            sorted(unsorted_tags.items(), key=lambda x: x[1], reverse=True)
+        )      
+
+    return tags
+
+# The next block of code is several date utilities
+# We need these because I've been sloppy about defining dates
+# TODO: Fix datetimes in all markdown files so this wouldn't be necessary
+def convert_dtstr_to_dt(date_str: str) -> datetime:
+    """
+    Convert a naive or non-naive date/datetime string to a datetime object.
+    Naive datetime strings are assumed to be in GMT (UTC) timezone.
+    """
+    try:
+        dt = parser.parse(date_str)
+        if dt.tzinfo is None:
+            # If the datetime object is naive, set it to GMT (UTC)
+            dt = dt.replace(tzinfo=pytz.UTC)
+        return dt
+    except (ValueError, TypeError) as e:
+        return None
+    
+def format_datetime(dt: datetime) -> str:
+    """Format the datetime object"""
+    formatted_date = dt.strftime("%B %d, %Y")
+    formatted_time = dt.strftime("%I:%M%p").lstrip('0').lower()
+    return f"{formatted_date} at {formatted_time}"
+
+# Most pages use a number of reusable components to 
+# render out HTML. I follow the FT Component standard
+# of TitleCasing these, even though they are functions.
+# That makes them easier to identify as FT Components.
+def Layout(title, socials, *tags):
+    """Generic layout for pages"""
+    return title, socials, (
+        Header(
+            A(Img(
+                cls='borderCircle', alt='Daniel Roy Greenfeld', src='/public/images/profile.jpg', width='108', height='108')
+                , href='/'),
+            A(H2('Daniel Roy Greenfeld'), href="/"),
+            P(
+                A('About', href='/about'),' | ', 
+                A('Articles', href='/posts'), ' | ',
+                A('Books', href='/books'), ' | ',
+                A('Jobs', href='/jobs'), ' | ',
+                A('Tags', href='/tags'), ' | ',
+                A('Search', href='/search')
+            ), style="text-align: center;"
+        ),
+    Main(*tags),
+    Footer(Hr(), P(
+                A('Mastodon', href='https://fosstodon.org/@danielfeldroy'), ' | ',
+                A('LinkedIn', href='https://www.linkedin.com/in/danielfeldroy/'), ' | ',
+                A('Twitter', href='https://twitter.com/pydanny'), ' | ',
+                A('Atom Feed', href='/feeds/atom.xml')
+            ),
+            P(f'All rights reserved {datetime.now().year}, Daniel Roy Greenfeld')
+        )
+    )
+
+def BlogPost(title: str, slug: str, timestamp: str, description: str):
+    """
+    This renders a blog posts short display used for the index, article list, and tags.
+    TODO: Rename to BlogPostPreview
+    """
+    return Span(
+                H2(A(title, href=f"/posts/{slug}")),
+                P(description, Br(), Small(Time(format_datetime(convert_dtstr_to_dt(timestamp))))),
+        )
+
+def Tag(slug: str):
+    """TODO: Rename to TagLink"""
+    return Span(A(slug, href=f"/tags/{slug}"), " ")
+
+def TagWithCount(slug: str, count: int):
+    """TODO: Rename to TagLinkWithCount"""
+    return Span(A(Span(slug), Small(f" ({count})"), href=f"/tags/{slug}"), " ")
+
+def MarkdownPage(slug: str):
+    """Renders a non-sequential markdown file"""
+    try:
+        text = pathlib.Path(f"pages/{slug}.md").read_text()
+    except FileNotFoundError:
+        return Response("Page not found", status_code=404) 
+    content = ''.join(text.split("---")[2:])
+    metadata = yaml.safe_load(text.split("---")[1])
+    return (Title(metadata.get('title', slug)),
+        Socials(site_name="https://daniel.feldroy.com",
+                        title=metadata.get('title', slug),
+                        description=metadata.get('description', 'slug'),
+                        url=f"https://daniel.feldroy.com/{slug}",
+                        image=metadata.get("image", default_social_image),
+                        ),                
+        A("← Back to home", href="/"),
+        Section(
+            Div(content,cls="marked")
+        )
+    )
+
+
+
 def Page404():
+    """404 view"""
     return FtResponse(Layout(Title("404 Not Found"),
         Socials(site_name="https://daniel.feldroy.com",
                     title="Daniel Roy Greenfeld",
@@ -42,8 +188,8 @@ exception_handlers = {
 
 app, rt = fast_app(hdrs=hdrs, pico=False, debug=True, exception_handlers=exception_handlers)
 
-@rt("/")
-def get():
+@rt
+def index():
     posts = [BlogPost(title=x["title"],slug=x["slug"],timestamp=x["date"],description=x.get("description", "")) for x in list_posts()]
     popular = [BlogPost(title=x["title"],slug=x["slug"],timestamp=x["date"],description=x.get("description", "")) for x in list_posts() if x.get("popular", False)]    
     return Layout(
@@ -87,10 +233,7 @@ def get():
 
 @rt("/posts/{slug}")
 def get(slug: str):
-    # post = [x for x in filter(lambda x: x["slug"] == slug, list_posts())][0]
     content, metadata = get_post(slug)
-    # content = pathlib.Path(f"posts/{slug}.md").read_text().split("---")[2]
-    # metadata = yaml.safe_load(pathlib.Path(f"posts/{slug}.md").read_text().split("---")[1])    
     tags = [Tag(slug=x) for x in metadata.get("tags", [])]
     specials = ()
     if 'TIL' in metadata['tags']:
